2ab6b774bedc1d46410cb0059c0508c400cc06eb
jjballano <jjballano@gmail.com>
2015-05-20 07:58:40 +0100

    Done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is just a project used to learn React Native.
 I followed this example: 
 http://www.raywenderlich.com/99473/introducing-react-native-building-apps-javascript?utm_source=javascriptweekly&utm_medium=email
+
+More references:
+https://facebook.github.io/react-native/
--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+var SearchPage = require('./searchPage');
 
-var ReactNativeExample = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+var {
+  AppRegistry, Text, StyleSheet, NavigatorIOS
+} = React;
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
-AppRegistry.registerComponent('ReactNativeExample', () => ReactNativeExample);
+var ReactNativeExampleApp = React.createClass({
+  render: function(){
+    return (
+        <NavigatorIOS style={styles.container} initialRoute={{
+          title: 'Property finder',
+          component: SearchPage,
+        }}/>
+    )
+  }
+});
+
+AppRegistry.registerComponent('ReactNativeExample', () => ReactNativeExampleApp );
